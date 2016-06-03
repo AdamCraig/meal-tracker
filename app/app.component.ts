ce438f3ed614492744a2a28857ba3d74c9e58f04
Adam Craig <lateralusajc@gmail.com>
2016-06-03 08:40:37 -0700

    Add initial contents of AppComponent, create Food class and instantiate AppComponent with an array of preset Food[] objects, set Click event emitter for clicking Food from the list and having the name of the food console logged

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,30 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Test</h1>
+    <div class="container">
+      <h1>Meal Tracker~</h1>
+      <h3 *ngFor="#food of foods" (click)="foodWasSelected(food)">
+        {{ food.name }}
+      </h3>
+    </div>
   `
 })
 export class AppComponent {
+  public foods: Food[];
+  constructor() {
+    this.foods = [
+      new Food("A Totally Real And Not-Just-For-Testing Cheeseburger", "Definitely not a hologram.", 700),
+      new Food("An Ice Cream Sundae Made of Nothing But Kitten Dreams", "For more testing purposes.", 12),
+      new Food("A Mountain of Ghost-Chocolate", "Tastes like nothing.", 4)
+    ];
+  }
+  foodWasSelected(clickedFood: Food): void {
+    console.log(clickedFood.name);
+  }
+}
 
+export class Food {
+  constructor(public name: string, public description: string, public calories: number) {
+
+  }
 }
